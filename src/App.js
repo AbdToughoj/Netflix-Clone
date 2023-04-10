d2446ddf54d70825ce039246d1ffb8eff3c82877
AbdToughoj <toughoj@gmail.com>
2023-04-10 20:17:19 +0000

    lab19

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import Home from "./Components/home/Home";
+import FavList from "./Components/favList/FavList";
+import NavBar from "./Components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favList" element={<FavList />} />
+      </Routes>
     </>
   );
-    
 }
 
 export default App;
